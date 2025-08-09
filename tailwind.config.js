3b08bbb2f32707f8fbf1f39d02d4c0858dec4979
Awifu <spendawifu@gmail.com>
2025-08-09 18:51:40 +0300

    feat(homepage): Initial Tailwind setup and basic homepage structure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.{html,js}",
+    "./public/**/*.{html,js}",
+    "./*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
